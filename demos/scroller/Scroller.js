ec922513ffee21e8874c32d42121b7f168aeadd3
KirbySaysHi <kirbysayshi@gmail.com>
2010-10-17 23:48:58 -0400

    cleaned up Scroller code

--- demos/scroller/Scroller.js
+++ demos/scroller/Scroller.js
@@ -39,64 +39,6 @@ Scroller.prototype.InitBodies = function(){
 	m.SetVertices(vs, 7, true);
 	this.bodies.NewMesh( m );
 	
-	//var p = new CRigidBody(2, 1);
-	//p.AddParticle( new CParticle( V3.$(1600, 80, 0),  10, 1000, 1 ) );
-	//p.AddParticle( new CParticle( V3.$(1700, 80, 0),  10, 1000, 1 ) );
-	//p.AddParticle( new CParticle( V3.$(1700, 240, 0), 10, 1000, 1 ) );
-	//p.AddParticle( new CParticle( V3.$(1600, 240, 0), 10, 1000, 1 ) );
-	//p.SetColor(1.0, 0.3, 0.3, 0.5);
-	//p.SetRigidBodyConstraints();
-	//this.bodies.NewBody(p);
-	
-	
-	//var p1 = new CRigidBody(2, 0.5);
-	//p1.AddParticle( new CParticle( V3.$(300, 10, 0),  2, 10, 0.5 ) );
-	//p1.SetColor(1.0, 0.3, 0.3, 0.5);
-	//p1.SetRigidBodyConstraints();
-	//this.bodies.NewBody(p1);
-	//
-	//var p2 = new CRigidBody(2, 0.5);
-	//p2.AddParticle( new CParticle( V3.$(300, 50, 0),  10, 10, 0.5 ) );
-	//p2.SetColor(1.0, 0.3, 0.3, 0.5);
-	//p2.SetRigidBodyConstraints();
-	//this.bodies.NewBody(p2);
-	//
-	//var p3 = new CRigidBody(2, 0.5);
-	//p3.AddParticle( new CParticle( V3.$(300, 90, 0),  15, 10, 0.5 ) );
-	//p3.SetColor(1.0, 0.3, 0.3, 0.5);
-	//p3.SetRigidBodyConstraints();
-	//this.bodies.NewBody(p3);
-	//
-	//var p4 = new CRigidBody(2, 0.5);
-	//p4.AddParticle( new CParticle( V3.$(300, 130, 0),  20, 10, 0.5 ) );
-	//p4.SetColor(1.0, 0.3, 0.3, 0.5);
-	//p4.SetRigidBodyConstraints();
-	//this.bodies.NewBody(p4);
-	
-	//var b1 = new CBox(V3.$(300, 10, 0), 10, 10, 11, 0.5);
-	//var b2 = new CBox(V3.$(300, 50, 0), 10, 10, 11, 0.5);
-	//var b3 = new CBox(V3.$(300, 90, 0), 10, 10, 11, 0.5);
-	//var b4 = new CBox(V3.$(300, 130, 0), 10, 10, 11, 0.5);
-	//
-	//this.bodies.NewBody(b1);
-	//this.bodies.NewBody(b2);
-	//this.bodies.NewBody(b3);
-	//this.bodies.NewBody(b4);
-	
-	// create rigid sphere
-	//var r = 10;
-	//var s = V3.$(100,20,0);
-	//var b = new CRigidBody(2, 2);
-	//for(var i = 0; i < 8; i++){
-	//	var t = (Math.PI * 2.0) * (i / 8);
-	//	var xPos = V3.add(s, V3.scale(V3.$(Math.cos(t), Math.sin(t), 0), r));
-	//	b.AddParticle( new CParticle( xPos, r, 3.5, 2 ) );
-	//}
-	//var b = new CBox(V3.$(800, 20, 0), 15, 1, 2, 1);
-	//b.SetRigidBodyConstraints();
-	//var b = new CSoftBody(V3.$(100, 20, 0), 20, 8, 3.5, 2, 1);
-	//b.SetColor(1.0, 0.3, 0.3, 0.5);
-	
 	var b = new Hero( V3.$(100, 20, 0) );
 	
 	var setup = [
@@ -115,15 +57,6 @@ Scroller.prototype.InitBodies = function(){
 	setup.forEach(function(e){
 		self.bodies.NewBody(e);
 	})
-	
-	//var b = new CRectangularBox(V3.$(200, 390, 0), 50, 50, 10, 11, 0.5);
-	
-	//var b = new CRigidBody(1, 1);
-	//b.AddParticle( new CParticle( V3.$(200, 390, 0), 10, 3.5, 0.5 ) );
-	//b.AddParticle( new CParticle( V3.$(205, 390, 0), 10, 3.5, 0.5 ) );
-	//b.AddParticle( new CParticle( V3.$(205, 395, 0), 10, 3.5, 0.5 ) );
-	//b.AddParticle( new CParticle( V3.$(200, 395, 0), 10, 3.5, 0.5 ) );
-	//b.SetRigidBodyConstraints();
 	
 	this.bodies.NewBody(b);
 	
@@ -176,10 +109,6 @@ Scroller.prototype.BasicMouseClickHandler = function(e){
 	var p = new CSoftBody( V3.add(pos, V3.scale(dir, this.scrollFocus.GetBoundingRad()))
 		, 3, 8, 5, 1, 0.5 );
 	
-	//var p = new CRigidBody(2, 0.5);
-	//p.AddParticle( new CParticle( V3.add(pos, V3.scale(dir, this.scrollFocus.GetBoundingRad())), 3, 10, 0.5 ) );
-	//p.SetColor(1.0, 0.3, 0.3, 0.5);
-	//p.SetRigidBodyConstraints();
 	this.bodies.NewBody(p);
 	p.AddForce( V3.scale(dir, 10000) );
 }
